91f3565822d53d66a37ef65a5e82b99815e054fc
Raquel Cruz <raquelicruz94@gmail.com>
2023-09-07 11:12:08 -0400

    got server running

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+const {sequelize} = require('./db');
+const {Entry} = require("./models/index")
+
+app.use(express.json());
+sequelize.sync();
+const routes = require('./routes/app');
+
+app.listen(port, () => {
+    console.log(`Server is listening on port http://localhost:${port}`)
+});
+
 
 /*
-
-
 const { db } = require('./models');
 const app = require('./routes/app');
 
