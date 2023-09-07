4ae83b53f937bb41e29389661a62dd771623c132
Raquel Cruz <raquelicruz94@gmail.com>
2023-09-07 11:13:00 -0400

    deleted comment

--- server.js
+++ server.js
@@ -12,28 +12,3 @@ app.listen(port, () => {
     console.log(`Server is listening on port http://localhost:${port}`)
 });
 
-
-/*
-const { db } = require('./models');
-const app = require('./routes/app');
-
-const PORT = process.env.PORT || 4000;
-
-const init = async () => {
-    try {
-        await db.sync();
-
-        app.listen(PORT, () => {
-            console.log(`Server listening at http://localhost:${PORT}`);
-
-        });
-
-
-    } catch(error){
-        console.error('Error starting server:', error)
-    }
-}; 
-
-init();
-
-*/
