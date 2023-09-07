02505f064acee5a8fa4b6475e54478bd46ca45fa
Raquel Cruz <raquelicruz94@gmail.com>
2023-09-07 10:26:46 -0400

    successfully seeded database

--- seed.js
+++ seed.js
@@ -1,11 +1,13 @@
-const { seedData } = require('./seedData');
+const seedData = require('./seedData');
 const { sequelize } = require('./db');
-
 const { Entry } = require('./models/index');
+
+
 
 const syncSeed = async () => {
     try {
-        await sequelize.sync({ force: true });
+       //const entries = await .findAll();
+        //await sequelize.sync({ force: true });
         await Entry.bulkCreate(seedData)
 
         console.log('db populated!');
