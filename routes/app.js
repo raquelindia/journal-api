77ddef9d48e822a5a02403a6c4650a915c69dcff
Raquel Cruz <raquelicruz94@gmail.com>
2023-09-08 21:36:24 -0400

    fixed post route

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const router = express.Router;
+const router = express.Router();
 const {Entry} = require('../models/index');
 const {sequelize} = require('../db');
 const {check, validationResult} = require("express-validator");
@@ -9,23 +9,23 @@ app.use(express.json());
 sequelize.sync();
 //const entriesRouter =
 
-//app.use("/entries", )
+app.use("/entries", router);
 
-app.get("/", async (request, response) => {
+router.get("/entries", async (request, response) => {
     const getEntries = await Entry.findAll();
     response.json(getEntries);
 });
 
-app.get("/:id", async (request, response) => {
+router.get("/:id", async (request, response) => {
     const id = request.params.id;
     const getOneEntry = await Entry.findByPk(id);
     response.json(getOneEntry);
 });
 
-app.post('/', async (request, response) =>{
-    const title = request.params.title;
-    const date = request.params.date;
-    const text = request.params.text;
+router.post('/', async (request, response) =>{
+    const title = request.body.title;
+    const date = request.body.date;
+    const text = request.body.text;
 
     const createEntry = await Entry.create({
         title: title,
@@ -36,7 +36,7 @@ response.json(createEntry);
 
 });
 
-app.put("/:id", async (request, response) => {
+router.put("/:id", async (request, response) => {
     const id = request.params.id;
     const editTitle = request.body.title;
     const editDate = request.body.date;
@@ -53,7 +53,7 @@ app.put("/:id", async (request, response) => {
 
 });
 
-app.delete('/:id', async (request, response) => {
+router.delete('/:id', async (request, response) => {
 const id = request.params.id;
 const foundEntry = await Entry.findByPk(id);
 const deleteEntry = await foundEntry.destroy();
@@ -63,4 +63,6 @@ response.json(deletedMessage);
 
 });
 
-module.exports = app;
+module.exports = {
+    app
+};
