07970d0f409fd1a0f00cd008fc6324f9483cd8aa
Raquel Cruz <raquelicruz94@gmail.com>
2023-09-15 16:16:06 -0400

    added more seed data to test

--- seedData.js
+++ seedData.js
@@ -25,20 +25,141 @@ module.exports = {
         creator: 'jenny'
     },
     {
-        title: 'Journal Entry #5', 
+        title: 'Jasmines first journal entry', 
         date: '2024-09-06',
         text: 'Started new TV show today',
-        creator: 'dean'
+        creator: 'jasmine'
+    },
+    {
+        title: 'Jasmines second journal entry', 
+        date: '2024-09-06',
+        text: 'Started new TV show today',
+        creator: 'jasmine'
+    },
+    {
+        title: 'Sheris first journal entry', 
+        date: '2024-09-07',
+        text: 'Started new TV show today',
+        creator: 'sheri'
+    },
+    {
+        title: 'Sheris second journal entry', 
+        date: '2024-09-06',
+        text: 'Started new TV show today',
+        creator: 'sheri'
+    },
+    {
+        title: 'Teris first journal entry', 
+        date: '2024-09-09',
+        text: 'Started new TV show today',
+        creator: 'teri'
+    },
+    {
+        title: 'Teris second journal entry', 
+        date: '2024-09-11',
+        text: 'Started new TV show today',
+        creator: 'teri'
+    },
+    {
+        title: 'Its my birthday!', 
+        date: '2024-09-15',
+        text: 'Started new TV show today',
+        creator: 'chris'
+    },
+    {
+        title: 'Chris second journal entry', 
+        date: '2024-09-017',
+        text: 'Started new TV show today',
+        creator: 'chris'
+    },
+    {
+        title: 'Tabrices first journal entry', 
+        date: '2024-09-16',
+        text: 'Started new TV show today',
+        creator: 'tabricedejanelleburns'
+    },
+    {
+        title: 'Tabrices second journal entry', 
+        date: '2024-09-26',
+        text: 'Started new TV show today',
+        creator: 'tabricedejanelleburns'
+    },
+    {
+        title: 'Masayas first journal entry', 
+        date: '2024-09-10',
+        text: 'Started new TV show today',
+        creator: 'masaya'
+    },
+    {
+        title: 'Masayas second journal entry', 
+        date: '2024-09-03',
+        text: 'Started new TV show today',
+        creator: 'masaya'
+    },
+    {
+        title: 'Tys first journal entry', 
+        date: '2024-09-27',
+        text: 'Started new TV show today',
+        creator: 'tellerbe16'
+    },
+    {
+        title: 'Tys second journal entry', 
+        date: '2024-09-27',
+        text: 'Started new TV show today',
+        creator: 'tellerbe16'
     }
+
 
     ],
 
     users: [
         {
     
-                username: "jenny",
-                name: "Jenny Smith",
-                email: "jennysmith@example.com"
+            username: "tellerbe16",
+            name: "Ty Ellerbe",
+            email: "tellerbe16@gmail.com"
+          
+        },
+        {
+    
+            username: "masaya",
+            name: "Masaya Diaz",
+            email: "masaya@gmail.com"
+          
+        },
+        {
+    
+            username: "tabricedejanelleburns",
+            name: "Tabrice Dejanelle Burns",
+            email: "tabricedejanelleburns@gmail.com"
+          
+        },
+        {
+        
+            username: "teri",
+            name: "Teri Cruz",
+            email: "teri@gmail.com"
+          
+        },
+        {
+    
+            username: "sheri",
+            name: "Sheri Cruz",
+            email: "sheri@gmail.com"
+          
+         },
+        {
+    
+            username: "chris",
+            name: "Chris Mack",
+            email: "chris@gmail.com"
+          
+        },
+        {
+    
+                username: "jasmine",
+                name: "Jasmine Rowe",
+                email: "jasmine@gmail.com"
               
         },
         {
